cb674efd9af45551aa5c38c11debdd0ffa7c477e
subhamkrguptadev <subham7031979@gmail.com>
2024-04-02 21:37:13 +0530

    first commit

---
+++ src/main/java/com/links/Personal/Links/PersonalLinksApplication.java
@@ -0,0 +1,13 @@
+package com.links.Personal.Links;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalLinksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalLinksApplication.class, args);
+	}
+
+}
